811ad9c7c6aa1373e8ae3390e917339936af6fb0
Cocker Lee <OuTine@Gmail.com>
2018-12-19 03:29:19 +0800

    究竟哪个起作用

---
+++ Cfms.Basic/Application/AppServiceControllerFeatureProvider.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper.Internal;
+using Cfms.Basic.Application.Services;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Cfms.Basic.Application
+{
+    public class AppServiceControllerFeatureProvider: ControllerFeatureProvider
+    {
+        protected override bool IsController(TypeInfo typeInfo)
+        {
+            var type = typeInfo.AsType();
+
+            if (!typeof(IAppService).IsAssignableFrom(type) ||
+                !typeInfo.IsPublic || typeInfo.IsAbstract || typeInfo.IsGenericType)
+            {
+                return false;
+            }
+
+            //var remoteServiceAttr = ReflectionHelper.GetSingleAttributeOrDefault<RemoteServiceAttribute>(typeInfo);
+
+            //if (remoteServiceAttr != null && !remoteServiceAttr.IsEnabledFor(type))
+            //{
+            //    return false;
+            //}
+
+            //var configuration = _iocResolver.Resolve<AbpAspNetCoreConfiguration>().ControllerAssemblySettings.GetSettingOrNull(type);
+            //return configuration != null && configuration.TypePredicate(type);
+            return base.IsController(typeInfo);
+        }
+    }
+}
