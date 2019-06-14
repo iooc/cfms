4ad854dbb12177f89e2083d4e0180089973854c3
Cocker Lee <OuTine@Gmail.com>
2019-06-15 00:17:11 +0800

    树查询模型

---
+++ Cfms.Basic/Application/Dto/TreeInputDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cfms.Basic.Application.Dto
+{
+    /// <summary>
+    /// 树形查询输入模型
+    /// </summary>
+    public class TreeInputDto<TPrimaryKey> : PagingInputDto 
+        where TPrimaryKey : struct
+    {
+        public TPrimaryKey? ParentId { get; set; }
+    }
+}
