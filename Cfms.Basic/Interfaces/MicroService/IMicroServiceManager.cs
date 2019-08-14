802e8346886579cd14a27b8d2a2fd1004bc5704e
Cocker Lee <OuTine@Gmail.com>
2019-08-15 00:47:47 +0800

    微服务问题，自实现，还是三方库

---
+++ Cfms.Basic/Interfaces/MicroService/IMicroServiceManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cfms.Basic.Interfaces.MicroService
+{
+    /// <summary>
+    /// 不依赖第三方工具自维护微服务声明接口
+    /// </summary>
+    interface IMicroServiceManager
+    {
+    }
+}
